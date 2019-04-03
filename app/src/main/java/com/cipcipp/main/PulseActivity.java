4a2d0ed30d546baa03987d836a1111fa52906e8e
nashihu <ahmad.n.id@gmail.com>
2019-04-03 10:17:26 +0700

    add auth

--- app/src/main/java/com/cipcipp/main/PulseActivity.java
+++ app/src/main/java/com/cipcipp/main/PulseActivity.java
@@ -1,5 +1,6 @@
 package com.cipcipp.main;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,7 +9,9 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cipcipp.main.Helper.CellListGenerator;
 import com.cipcipp.main.Helper.OpenApp;
@@ -19,7 +22,12 @@ import com.cipcipp.main.TableEngine.ProviderAdapter;
 import com.evrencoskun.tableview.TableView;
 import com.cipcipp.main.TableEngine.MyTableViewAdapter;
 import com.cipcipp.main.TableEngine.MyTableViewListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -43,6 +51,11 @@ public class PulseActivity extends AppCompatActivity implements ProviderAdapter.
     private ArrayList<String> provider_title = new ArrayList<>();
     private ArrayList<Integer> provider_id = new ArrayList<>();
     private List<List<CellModel>> price_test = new ArrayList<>();
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private EditText email;
+    private EditText password;
+    private FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,62 +67,46 @@ public class PulseActivity extends AppCompatActivity implements ProviderAdapter.
         report = findViewById(R.id.report);
         check_report = findViewById(R.id.report_check);
         title = getIntent().getStringExtra("title");
-        FirebaseDatabase database;
-        DatabaseReference databaseReference;
+        mAuth = FirebaseAuth.getInstance();
+        signInListener();
+        signOutListener();
+        signUpListener();
+        authStateListener();
         DatabaseHandler db = new DatabaseHandler(PulseActivity.this);
         db.reCreateTable();
         if(db.getAllContacts().size()!=0) {
             setupData(db);
             renderData();
         } else {
-            FirebaseApp.initializeApp(this);
-            database = FirebaseDatabase.getInstance();
-            FirebaseDatabase.getInstance().getReference().child(title);
-            databaseReference = database.getReference("testing").child(title); //Telkomsel,etc
-            databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    DatabaseHandler db = new DatabaseHandler(PulseActivity.this);
-                    RowCells rowCells = new RowCells();
-                    ArrayList<CellModel> colValz = new ArrayList<>();
-                    for (DataSnapshot child_i : dataSnapshot.getChildren()) {
-                        String rownum = child_i.getKey();
-                        ArrayList<CellModel> price_= new ArrayList<>();
-                        for(int i =0;i<(child_i.getChildrenCount());i++) {
-                            if(rownum!=null) {
-                                price_.add(new CellModel(String.valueOf(i),dataSnapshot.child(rownum).child(String.valueOf(i)).getValue()));
-                                if(rownum.equals("nominal")) {
-                                    colValz.add(new CellModel(String.valueOf(i),dataSnapshot.child(rownum).child(String.valueOf(i)).getValue()));
-                                }
-                            }
-                        }
-                        if(rownum!=null && !rownum.equals("updatedAt")) {
-                            if(rownum.equals("nominal")){
-                                rowCells.bulkSetter(rowCells,colValz);
-                                db.addColVal(rowCells);
-                            } else {
-                                rowCells.bulkSetter(rowCells,price_);
-                                db.addRowCells(rowCells,rownum);
-                            }
-                        } else {
-                            if(rownum!=null) {
-                                ArrayList<CellModel> rownumm = new ArrayList<>();
-                                rownumm.add(new CellModel("0",dataSnapshot.child(rownum).getValue()));
-                                rowCells.bulkSetter(rowCells,rownumm);
-                                db.addRowCells(rowCells,"updatedAt");
-                            }
-                        }
-                    }
-                    setupData(db);
-                    renderData();
-//                    Toast.makeText(PulseActivity.this,"berhasil load data dari server",Toast.LENGTH_LONG).show();
-                }
-                @Override
-                public void onCancelled(@NonNull DatabaseError databaseError) {
-                    Log.d("TAGGG","ga nemu coy");
-                }
-            });
+            firebaseUser = mAuth.getCurrentUser();
+            if(firebaseUser!=null) {
+                fetchData();
+            } else {
+                String noUser = "please sign in first";
+                textView.setText(noUser);
+                textView.setVisibility(View.VISIBLE);
+                findViewById(R.id.reportGroup).setVisibility(View.GONE);
+                AuthField(View.VISIBLE);
+            }
+
         }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        DatabaseHandler db = new DatabaseHandler(PulseActivity.this);
+        db.reCreateTable();
+        if(mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+
     }
 
     @Override
@@ -176,5 +173,167 @@ public class PulseActivity extends AppCompatActivity implements ProviderAdapter.
             provider_id.add(R.mipmap.ic_launcher);
             provider_title.add(rowNumz.get(i));
         }
+    }
+
+    private void signInListener() {
+
+        findViewById(R.id.sign_in).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                email = findViewById(R.id.email);
+                password = findViewById(R.id.pass);
+                String emailString = email.getText().toString();
+                String pwd = password.getText().toString();
+
+                if(!emailString.equals("") && !pwd.equals("")) {
+                    mAuth.signInWithEmailAndPassword(emailString,pwd)
+                            .addOnCompleteListener(PulseActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (!task.isSuccessful()) {
+                                        Toast.makeText(PulseActivity.this, "Failed sign in !",Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        Toast.makeText(PulseActivity.this, "sign in Success!",Toast.LENGTH_SHORT).show();
+                                        String successLogin = "Loading...";
+                                        textView.setText(successLogin);
+                                        fetchData();
+                                        findViewById(R.id.contentGroup).setVisibility(View.VISIBLE);
+                                        AuthField(View.GONE);
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+    }
+
+    private void signOutListener() {
+        findViewById(R.id.sign_out).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                Intent backToHome = new Intent(PulseActivity.this,ActivityMain.class);
+                startActivity(backToHome);
+                Toast.makeText(PulseActivity.this, "sign out success", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void signUpListener() {
+        findViewById(R.id.sign_up).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                email = findViewById(R.id.email);
+                password = findViewById(R.id.pass);
+                String emailString = email.getText().toString();
+                String pwd = password.getText().toString();
+                if(!emailString.equals("") && !pwd.equals("")) {
+                    mAuth.createUserWithEmailAndPassword(emailString,pwd).addOnCompleteListener(PulseActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            String emailString = email.getText().toString();
+                            String pwd = password.getText().toString();
+                            if(!emailString.equals("") && !pwd.equals("")) {
+                                mAuth.signInWithEmailAndPassword(emailString,pwd)
+                                        .addOnCompleteListener(PulseActivity.this, new OnCompleteListener<AuthResult>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                                if (!task.isSuccessful()) {
+                                                    Toast.makeText(PulseActivity.this, "Failed sign in..",Toast.LENGTH_SHORT).show();
+                                                } else {
+                                                    Toast.makeText(PulseActivity.this, "sign up Success!",Toast.LENGTH_SHORT).show();
+                                                    String successLogin = "Loading...";
+                                                    textView.setText(successLogin);
+                                                    fetchData();
+                                                    findViewById(R.id.contentGroup).setVisibility(View.VISIBLE);
+                                                    AuthField(View.GONE);
+                                                }
+                                            }
+                                        });
+                            }
+                        }
+                    });
+                }
+
+            }
+        });
+    }
+
+    private void fetchData() {
+        FirebaseDatabase database;
+        DatabaseReference databaseReference;
+        FirebaseApp.initializeApp(this);
+        database = FirebaseDatabase.getInstance();
+        FirebaseDatabase.getInstance().getReference().child(title);
+        databaseReference = database.getReference("testing").child(title); //Telkomsel,etc
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                DatabaseHandler db = new DatabaseHandler(PulseActivity.this);
+                db.reCreateTable();
+                RowCells rowCells = new RowCells();
+                ArrayList<CellModel> colValz = new ArrayList<>();
+                for (DataSnapshot child_i : dataSnapshot.getChildren()) {
+                    String rownum = child_i.getKey();
+                    ArrayList<CellModel> price_= new ArrayList<>();
+                    for(int i =0;i<(child_i.getChildrenCount());i++) {
+                        if(rownum!=null) {
+                            price_.add(new CellModel(String.valueOf(i),dataSnapshot.child(rownum).child(String.valueOf(i)).getValue()));
+                            if(rownum.equals("nominal")) {
+                                colValz.add(new CellModel(String.valueOf(i),dataSnapshot.child(rownum).child(String.valueOf(i)).getValue()));
+                            }
+                        }
+                    }
+                    if(rownum!=null && !rownum.equals("updatedAt")) {
+                        if(rownum.equals("nominal")){
+                            rowCells.bulkSetter(rowCells,colValz);
+                            db.addColVal(rowCells);
+                        } else {
+                            rowCells.bulkSetter(rowCells,price_);
+                            db.addRowCells(rowCells,rownum);
+                        }
+                    } else {
+                        if(rownum!=null) {
+                            ArrayList<CellModel> rownumm = new ArrayList<>();
+                            rownumm.add(new CellModel("0",dataSnapshot.child(rownum).getValue()));
+                            rowCells.bulkSetter(rowCells,rownumm);
+                            db.addRowCells(rowCells,"updatedAt");
+                        }
+                    }
+                }
+                setupData(db);
+                renderData();
+                findViewById(R.id.sign_out).setVisibility(View.VISIBLE);
+                findViewById(R.id.reportGroup).setVisibility(View.VISIBLE);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d("TAGGG","ga nemu coy");
+            }
+        });
+
+    }
+
+    private void authStateListener() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    //user is signed in
+                    Log.d("Firebase Auth","user signed in");
+                } else {
+                    //user is signed out
+                    Log.d("Firebase Auth","user signed out");
+                }
+            }
+        };
+    }
+
+    private void AuthField(int state) {
+        findViewById(R.id.email).setVisibility(state);
+        findViewById(R.id.pass).setVisibility(state);
+        findViewById(R.id.sign_in).setVisibility(state);
+        findViewById(R.id.sign_up).setVisibility(state);
     }
 }
