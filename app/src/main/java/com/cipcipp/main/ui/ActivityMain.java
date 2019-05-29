c464b19b69e79e56b84ef679a0cacf3f47a17dce
nashihu <ahmad.n.id@gmail.com>
2019-05-29 13:16:55 +0700

    GDK submission purpose

--- app/src/main/java/com/cipcipp/main/ui/ActivityMain.java
+++ app/src/main/java/com/cipcipp/main/ui/ActivityMain.java
@@ -41,7 +41,6 @@ public class ActivityMain extends AppCompatActivity implements View.OnClickListe
         smartfren.setOnClickListener(this);
         cipvideo = findViewById(R.id.cipcipphow);
         cipvideo.setOnClickListener(this);
-        cipvideo.setVisibility(View.GONE);
     }
     @Override
     public void onClick(View view) {
@@ -85,7 +84,7 @@ public class ActivityMain extends AppCompatActivity implements View.OnClickListe
                 startActivity(moveIntent);
                 break;
             case R.id.cipcipphow:
-                String url = "http://www.youtube.com/nashihu";
+                String url = "http://www.github.com/nashihu/cipcipp";
                 Uri uri = Uri.parse(url); // missing 'http://' will cause crashed
                 Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                 startActivity(intent);
